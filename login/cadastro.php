653ed80fbee53a4cc9058fb109819c56467a332b
Brunob0797 <31026046+Brunob0797@users.noreply.github.com>
2017-12-04 12:09:48 -0200

    Create cadastro.php

---
+++ login/cadastro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Cadastro</title>
+</head>
+
+<body>
+<center>
+<form action="inserir.php" method="post">
+<p><b>FORMULÁRIO PARA CADASTRO</b></p>
+Login:<input type="text" name="login" maxlength="20" /><br />
+Senha:<input type="password" name="senha" maxlength="20"/><br /><br />
+<input type="submit" value="Enviar" />
+</form>
+</center>
+</body>
+</html>
